3b4259ff79884f5d9f0f9ad120817b083391d4e7
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-18 14:14:25 -0300

    cors
    cors

--- api/downloadPdf.php
+++ api/downloadPdf.php
@@ -1,8 +1,13 @@
 <?php
 
-header("Access-Control-Allow-Origin: *"); 
-header("Access-Control-Allow-Methods: POST"); 
-header("Access-Control-Allow-Headers: Content-Type"); 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS"); 
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);

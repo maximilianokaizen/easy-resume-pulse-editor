1514e9fccc88a6b48d96479af3524e9976770818
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-20 09:10:01 -0300

    Update downloadPdf.php

--- api/downloadPdf.php
+++ api/downloadPdf.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once('/lib/enabledCors.php');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS"); 
+header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);

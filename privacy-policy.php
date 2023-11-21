7f75d28f01ddac133438787b9c06a6bb683d58e0
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-21 14:50:23 -0300

    privaci-policy
    privaci-policy

--- privacy-policy.php
+++ privacy-policy.php
@@ -18,8 +18,28 @@ include_once 'init.php';
   <div class="container" data-aos="fade-up">
 
     <div class="section-title">
-      <h2>Login</h2>
-      <p>Enter your credentials to access your account.</p>
+      
+    <h1>Privacy Policy</h1>
+
+<p>This Privacy Policy describes how [Name of the Application] ("we," "our," or "the application") collects, uses, and shares personal information from users ("you" or "user") who use our web application and related services.</p>
+
+<h2>Information We Collect</h2>
+
+<ul>
+  <li><strong>Personal Information:</strong> We may collect personal information voluntarily provided by you when using our application, such as name, email address, contact information, and profile details.</li>
+  <li><strong>Usage Information:</strong> We may automatically collect information about how you interact with our application, including data about your device, IP address, browser type, pages visited, visit duration, and usage patterns.</li>
+</ul>
+
+<h2>Use of Information</h2>
+
+<p><strong>Enhancing User Experience:</strong> We use the collected information to provide and improve our services, customize your experience, and respond to your inquiries or requests.</p>
+<p><strong>Communication:</strong> We may use your contact information to send you updates, newsletters, or important notices about our services, but you can opt out of these communications at any time.</p>
+
+<h2>Sharing of Information</h2>
+
+<p>[Insert information about how and with whom the collected data is shared, if applicable.]</p>
+
+<p>This is a general overview of our privacy practices. For more detailed information, please review our complete Privacy Policy.</p>
     </div>
 
     <div class="row mt-5">

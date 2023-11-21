f139752fae9c1508c00224e7d4ebb0d5302478ee
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-21 15:14:56 -0300

    Create services-conditions.php

---
+++ services-conditions.php
@@ -0,0 +1,48 @@
+<?php
+include_once 'init.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include_once 'head.php'; ?>
+</head>
+<body>
+<?php include_once 'header.php'; ?>
+  <main id="main">
+    
+  <div class="container" data-aos="fade-up">
+
+    
+  
+
+  <section>
+      <h2>1. Acceptance of Terms</h2>
+      <p>By accessing this website, you agree to comply with these terms and conditions.</p>
+    </section>
+
+    <section>
+      <h2>2. Use of Services</h2>
+      <p>You agree to use the services provided on this website responsibly and in accordance with applicable laws and regulations.</p>
+    </section>
+
+    <section>
+      <h2>3. User Conduct</h2>
+      <p>Users are responsible for their actions while using this website and must not engage in any unlawful or prohibited activities.</p>
+    </section>
+
+    <!-- Add more sections for specific terms and conditions -->
+
+    <section>
+      <h2>10. Contact Information</h2>
+      <p>If you have any questions regarding these terms, please contact us at [contact@email.com].</p>
+    </section>
+    
+  </div>
+     
+  </main><!-- End #main -->
+  <?php include_once 'footer.php'; ?>
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <?php include_once('footer_js_css.php') ?>
+</body>
+</html>

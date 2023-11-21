519886940927d8c38e29c56c1c28c7c1d94d5b3a
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-21 16:22:09 -0300

    upps
    upps

--- login/google.php
+++ login/google.php
@@ -1,15 +1,33 @@
 <?php
+require_once 'vendor/autoload.php'; // Ruta a tu archivo autoload de la biblioteca google/apiclient
 
-require_once 'vendor/autoload.php';
+// Configuración de la autenticación
+$client = new Google_Client();
+$client->setClientId('223124831209-p04fqq68spt1pm60j69drbrcgknjsnl5.apps.googleusercontent.com');
+$client->setClientSecret('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');
 
-$client = new Google_Client(['client_id' => '223124831209-p04fqq68spt1pm60j69drbrcgknjsnl5.apps.googleusercontent.com']);  
-$payload = $client->verifyIdToken($id_token);
-if ($payload) {
-  $userid = $payload['sub'];
-  // If request specified a G Suite domain:
-  //$domain = $payload['hd'];
-  die(var_dump($payload));
+// Verificar si hay un código de autorización
+if (isset($_GET['code'])) {
+    $client->authenticate($_GET['code']); // Intercambiar código por token de acceso
+    $_SESSION['access_token'] = $client->getAccessToken(); // Almacenar el token en la sesión o en una base de datos
+}
+
+// Verificar si hay un token de acceso almacenado
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+    $client->setAccessToken($_SESSION['access_token']); // Configurar el token almacenado en el cliente
+
+    // Crear servicio de la API de Google que quieres usar (por ejemplo, Gmail API)
+    $service = new Google_Service_Gmail($client);
+
+    // Ejemplo: Obtener información del perfil del usuario
+    $userInfo = $service->userinfo->get();
+
+    // Mostrar información del usuario
+    echo 'ID de usuario: ' . $userInfo->getId() . '<br>';
+    echo 'Nombre: ' . $userInfo->getName() . '<br>';
+    echo 'Email: ' . $userInfo->getEmail() . '<br>';
 } else {
-  // Invalid ID token
-  die('redirect...');
+    // Si no hay token de acceso, redirigir al usuario a la página de autenticación de Google
+    $authUrl = $client->createAuthUrl();
+    header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
 }
---
+++ login/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'google/apiclient' => array(
+            'pretty_version' => '1.1.3',
+            'version' => '1.1.3.0',
+            'reference' => '523430f21a92c54172099d12d0f31d40f3de67e9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../google/apiclient',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

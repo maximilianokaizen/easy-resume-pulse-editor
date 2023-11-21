ee49c560d7aa66b07dd378eb6d62d0e732035e4a
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-21 16:31:41 -0300

    Update google.php
    sape

--- login/google.php
+++ login/google.php
@@ -4,40 +4,38 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'vendor/autoload.php'; // Ruta a tu archivo autoload de la biblioteca google/apiclient
+require_once 'vendor/autoload.php'; 
+require_once('../api/lib/token/TokenManager.php');
 
 $client = new Google_Client();
 $client->setClientId('223124831209-p04fqq68spt1pm60j69drbrcgknjsnl5.apps.googleusercontent.com');
 $client->setClientSecret('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');
 
-var_dump($_GET);
-var_dump($_POST);
+$token = $_POST['credential'];
 
-die('apa');
+die($token);
+
 /*
-// Verificar si hay un código de autorización
-if (isset($_GET['code'])) {
-    $client->authenticate($_GET['code']); // Intercambiar código por token de acceso
-    $_SESSION['access_token'] = $client->getAccessToken(); // Almacenar el token en la sesión o en una base de datos
-}
-
-// Verificar si hay un token de acceso almacenado
-if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-    $client->setAccessToken($_SESSION['access_token']); // Configurar el token almacenado en el cliente
-
-    // Crear servicio de la API de Google que quieres usar (por ejemplo, Gmail API)
-    $service = new Google_Service_Gmail($client);
-
-    // Ejemplo: Obtener información del perfil del usuario
-    $userInfo = $service->userinfo->get();
-
-    // Mostrar información del usuario
-    echo 'ID de usuario: ' . $userInfo->getId() . '<br>';
-    echo 'Nombre: ' . $userInfo->getName() . '<br>';
-    echo 'Email: ' . $userInfo->getEmail() . '<br>';
-} else {
-    // Si no hay token de acceso, redirigir al usuario a la página de autenticación de Google
-    $authUrl = $client->createAuthUrl();
-    header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+{
+  "iss": "https://accounts.google.com",
+  "azp": "223124831209-p04fqq68spt1pm60j69drbrcgknjsnl5.apps.googleusercontent.com",
+  "aud": "223124831209-p04fqq68spt1pm60j69drbrcgknjsnl5.apps.googleusercontent.com",
+  "sub": "108061482042918611723",
+  "email": "maximilianokaizen@gmail.com",
+  "email_verified": true,
+  "nbf": 1700594478,
+  "name": "Maximiliano Rossi",
+  "picture": "https://lh3.googleusercontent.com/a/ACg8ocKyi8Jnc4Y3Bhyq1nQjizIDg7EbKbdz2a2e0n4bGdBW=s96-c",
+  "given_name": "Maximiliano",
+  "family_name": "Rossi",
+  "locale": "es-419",
+  "iat": 1700594778,
+  "exp": 1700598378,
+  "jti": "187415738fb18f7a1a8fe80fde24ff46e53f929c"
 }
 */
+
+
+
+
+

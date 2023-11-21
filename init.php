62b7e80057e2d9e58f58285163f9150c880b87e9
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-21 15:02:36 -0300

    asd
    asd

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){
     // prod
-    $baseUrl = 'https://easyresumepulse.com';
+    $baseUrl = 'https://easyresumepulse.com/en';
 } else {
     // local    
     $baseUrl = 'http://localhost:8080';
--- privacy-policy.php
+++ privacy-policy.php
@@ -9,17 +9,11 @@ include_once 'init.php';
 <body>
 <?php include_once 'header.php'; ?>
   <main id="main">
-    <section class="inner-page page-content">
-      <div class="container">
-       <div class="content-center">
-            <h3>Login or Register</h3>
-        <!-- login or register -->
-        <section id="login" class="login">
+    
   <div class="container" data-aos="fade-up">
 
-    <div class="section-title">
-      
-    <h1>Privacy Policy</h1>
+    
+  <h1>Privacy Policy</h1>
 
 <p>This Privacy Policy describes how [Name of the Application] ("we," "our," or "the application") collects, uses, and shares personal information from users ("you" or "user") who use our web application and related services.</p>
 
@@ -40,43 +34,10 @@ include_once 'init.php';
 <p>[Insert information about how and with whom the collected data is shared, if applicable.]</p>
 
 <p>This is a general overview of our privacy practices. For more detailed information, please review our complete Privacy Policy.</p>
-    </div>
 
-    <div class="row mt-5">
-      <div class="col-lg-6 offset-lg-3">
-
-        <form action="login.php" method="post" role="form" class="php-login-form">
-          <div class="row gy-2">
-            <div class="col-12 form-group">
-              <input type="email" name="email" class="form-control" id="email" placeholder="Your Email" required>
-            </div>
-            <div class="col-12 form-group">
-              <input type="password" class="form-control" name="password" id="password" placeholder="Your Password" required>
-            </div>
-            <div class="col-12">
-              <button type="submit" class="btn btn-primary">Login</button>
-            </div>
-          </div>
-        </form>
-
-        <div class="separator mt-4 mb-3 text-center">OR</div>
-
-        <!-- Botón para iniciar sesión con Facebook -->
-        <div class="text-center">
-          <a href="login-with-facebook.php" class="btn btn-facebook">Login with Google</a>
-          <a href="login-with-facebook.php" class="btn btn-facebook">Login with Facebooke</a>
-        </div>
-
-      </div>
-    </div>
-
+      
   </div>
-</section><!-- End Login Section -->
-
-        <!-- end of login or register -->
-       </div>
-      </div>
-    </section>
+     
   </main><!-- End #main -->
   <?php include_once 'footer.php'; ?>
   <div id="preloader"></div>

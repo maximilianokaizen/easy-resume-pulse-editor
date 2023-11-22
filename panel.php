cf23567b33bce4a1444a688435495a8d283f96de
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-22 16:59:49 -0300

    get resumes
    get resumes

--- panel.php
+++ panel.php
@@ -5,6 +5,39 @@ include_once 'init-panel.php';
 <html lang="en">
 <head>
 <?php include_once 'head.php'; ?>
+<script>
+try {
+
+  const token = sessionStorage.getItem('token');
+  const uuid = sessionStorage..getItem('uuid');
+
+  if (!token || !uuid) {
+    throw new Error('No se encontraron el token o el uuid en el almacenamiento.');
+  }
+
+  const url = `<?=$baseUrl?>/api/resumes/listResumes.php?token=${token}&uuid=${uuid}`;
+
+  // Realizar la petición GET
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Mostrar el resultado en la consola
+    console.log(data);
+  })
+  .catch(error => {
+    // Capturar y manejar errores de la petición
+    console.error('Error en la petición:', error);
+  });
+} catch (error) {
+  // Capturar y manejar errores generales
+  console.error('Error:', error);
+}
+</script>
 </head>
 <body>
 <?php include_once 'header-panel.php'; ?>

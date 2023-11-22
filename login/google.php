a20e77168c6be0a43df445a7f71d973d46d814a1
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-22 16:01:19 -0300

    Update google.php

--- login/google.php
+++ login/google.php
@@ -1,100 +1,100 @@
 <?php
 
-require_once 'vendor/autoload.php'; 
-require_once('../api/lib/token/TokenManager.php');
-require_once('../api/internal/users/Users.php');
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once 'vendor/autoload.php'; 
+require_once('../api/lib/token/TokenManager.php');
+require_once('../api/internal/users/Users.php');
+
 use Lcobucci\JWT\Parser;
 
-try {
-  
-  $uuidString = generateUUIDv4();
-  $minutes = 180;
-  
-  $envFile = __DIR__ . '/.env';
-  $env = parse_ini_file($envFile);
-  
-  if ($env['ENVIRONMENT'] !== 'LOCAL'){
-    $baseUrl = 'https://easyresumepulse.com/en';
-  }else{
-    $baseUrl = 'http://localhost:8080';
-  }
-  
-  if ($env['ENVIRONMENT'] !== 'LOCAL'){
-    $client = new Google_Client();
-    $client->setClientId('223124831209-p04fqq68spt1pm60j69drbrcgknjsnl5.apps.googleusercontent.com');
-    $client->setClientSecret('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');
-    $token = $_POST['credential'];
-    $token = (new Parser())->parse((string) $token); // Parses from a string
-    $claims = $token->getClaims();
-    $email = $claims['email']->getValue();
-    $email_verified =  $claims['email_verified']->getValue();
-    $name = $claims['name']->getValue();
-    $picture =  $claims['picture']->getValue();
-    $given_name = $claims['given_name']->getValue();
-    $family_name =  $claims['family_name']->getValue();
-  }else{
-    $email = 'rossi.maxi@gmail.com';
-    $email_verified = true;
-    $name = 'Maximiliano Rossi';
-    $picture =  '';
-    $given_name = 'Maximiliano';
-    $family_name =  'Rossi';
-  }
-  
-  $user = new User();
-  if (!$user->exist($email)) {
-    // register
-    $randomPassword = password_hash(base64_encode(random_bytes(8)), PASSWORD_DEFAULT);
-    $premium = false;
-    $plan = 'free';
-    $social = true;
-    $socialName = 'google';
-    $active = 1;
-    $registrationResult = $user->register(
-      $uuidString, 
-      $email,
-      $randomPassword, 
-      $given_name,
-      $family_name,
-      0, // premium
-      $plan,
-      1, // social
-      $socialName,
-      $active,
-    );
-    if ($registrationResult) {
-      // generate JWT for this user
-      $token = TokenManager::generateTokenSocial($uuidString, $minutes);
-      header('Location: ' . $baseUrl . '/provisionalLogin.php?token=' . $token . '&uuid=' . $uuidString);
-    }else{
-      header('Location: ' . $baseUrl . '/index.php?err=1&code=001');
-    }
-  }else{
-    // registered user
-    // generate JWT for this user
-    $uuid = $user->getUuidByEmail($email);
-    $token = TokenManager::generateTokenSocial($uuid, $minutes);
-    $urlToRedirect = $baseUrl . '/provisionalLogin.php?token=' . $token . '&uuid=' . $uuid;
-    if ($uuid === NULL){
-      header('Location: ' . $baseUrl . '/index.php?err=1&code=002');
-    }else{
-      header('Location: ' . $urlToRedirect);
-    }
-  }
-  
-  function generateUUIDv4() {
-    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-        mt_rand(0, 0x0fff) | 0x4000,
-        mt_rand(0, 0x3fff) | 0x8000,
-        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-    );
-  }
-} catch (Exception $e) {
-  die($e->getMessage());
+$uuidString = generateUUIDv4();
+$minutes = 180;
+
+$envFile = __DIR__ . '/.env';
+$env = parse_ini_file($envFile);
+
+if ($env['ENVIRONMENT'] !== 'LOCAL'){
+  $baseUrl = 'https://easyresumepulse.com/en';
+}else{
+  $baseUrl = 'http://localhost:8080';
 }
+
+if ($env['ENVIRONMENT'] !== 'LOCAL'){
+  $client = new Google_Client();
+  $client->setClientId('223124831209-p04fqq68spt1pm60j69drbrcgknjsnl5.apps.googleusercontent.com');
+  $client->setClientSecret('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');
+  $token = $_POST['credential'];
+  $token = (new Parser())->parse((string) $token); // Parses from a string
+  $claims = $token->getClaims();
+  $email = $claims['email']->getValue();
+  $email_verified =  $claims['email_verified']->getValue();
+  $name = $claims['name']->getValue();
+  $picture =  $claims['picture']->getValue();
+  $given_name = $claims['given_name']->getValue();
+  $family_name =  $claims['family_name']->getValue();
+}else{
+  $email = 'rossi.maxi@gmail.com';
+  $email_verified = true;
+  $name = 'Maximiliano Rossi';
+  $picture =  '';
+  $given_name = 'Maximiliano';
+  $family_name =  'Rossi';
+}
+
+$user = new User();
+if (!$user->exist($email)) {
+  // register
+  $randomPassword = password_hash(base64_encode(random_bytes(8)), PASSWORD_DEFAULT);
+  $premium = false;
+  $plan = 'free';
+  $social = true;
+  $socialName = 'google';
+  $active = 1;
+  $registrationResult = $user->register(
+    $uuidString, 
+    $email,
+    $randomPassword, 
+    $given_name,
+    $family_name,
+    0, // premium
+    $plan,
+    1, // social
+    $socialName,
+    $active,
+  );
+  if ($registrationResult) {
+    // generate JWT for this user
+    $token = TokenManager::generateTokenSocial($uuidString, $minutes);
+    header('Location: ' . $baseUrl . '/provisionalLogin.php?token=' . $token . '&uuid=' . $uuidString);
+  }else{
+    header('Location: ' . $baseUrl . '/index.php?err=1&code=001');
+  }
+}else{
+  // registered user
+  // generate JWT for this user
+  $uuid = $user->getUuidByEmail($email);
+  $token = TokenManager::generateTokenSocial($uuid, $minutes);
+  $urlToRedirect = $baseUrl . '/provisionalLogin.php?token=' . $token . '&uuid=' . $uuid;
+  if ($uuid === NULL){
+    header('Location: ' . $baseUrl . '/index.php?err=1&code=002');
+  }else{
+    header('Location: ' . $urlToRedirect);
+  }
+}
+
+function generateUUIDv4() {
+  return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+      mt_rand(0, 0x0fff) | 0x4000,
+      mt_rand(0, 0x3fff) | 0x8000,
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+  );
+}
+
+
+
+
+

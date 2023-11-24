6dedc1c89269a829c094959f5ec7174b54a34493
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-24 15:33:06 -0300

    sip
    sip

--- editor/editor.php
+++ editor/editor.php
@@ -19,7 +19,6 @@ if (empty($_GET['token']) || empty($_GET['uuid']) || empty($_GET['template'])) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <div class="container d-flex align-items-center justify-content-between">
 
       <h1 class="logo"><a href="<?=$baseUrl?>">
-      <img src="<?=$baseUrl?>/assets/logo-small.png" alt=""/>EasyResumePulse<span class="green-color">.com</span></a></h1>
+      <img src="<?=$baseUrl?>/assets/logo-small.png" alt="" class="logo-header" />EasyResumePulse<span class="green-color">.com</span></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 

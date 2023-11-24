4fe12446b2043f784fe656756ec5b4285dce4e4b
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-24 15:21:00 -0300

    asdasd
    asdasd

--- header-panel.php
+++ header-panel.php
@@ -3,7 +3,7 @@
     <div class="container d-flex align-items-center justify-content-between">
 
       <h1 class="logo"><a href="<?=$baseUrl?>">
-      <img src="/assets/logo-small.png" alt=""/>
+      <img src="<?=$baseUrl?>/assets/logo-small.png" alt=""/>
       EasyResumePulse<span class="green-color">.com</span></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
@@ -11,7 +11,6 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="<?=$baseUrl?>">Home</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Feedback / Contact</a></li>
           <li><a class="getstarted scrollto show-ui" href="#" id="signOut">Logout</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <div class="container d-flex align-items-center justify-content-between">
 
       <h1 class="logo"><a href="<?=$baseUrl?>">
-      <img src="/assets/logo-small.png" alt=""/>EasyResumePulse<span class="green-color">.com</span></a></h1>
+      <img src="<?=$baseUrl?>/assets/logo-small.png" alt=""/>EasyResumePulse<span class="green-color">.com</span></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -12,7 +12,6 @@
           <li><a class="nav-link scrollto active" href="<?=$baseUrl?>">Home</a></li>
           <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Feedback / Contact</a></li>
           <li><a class="getstarted scrollto btn-get-green" href="<?=$baseUrl?>/signin.php">Create Resume Now</a></li>
           <li><a class="getstarted scrollto show-ui" href="<?=$baseUrl?>/signin.php">Sing in / Register</a></li>
         </ul>

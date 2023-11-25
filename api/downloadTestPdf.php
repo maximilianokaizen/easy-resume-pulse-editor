0219961ae776a1d0f06687d0539eba79876b0a7d
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-25 12:32:35 -0300

    asdas
    das

--- api/downloadTestPdf.php
+++ api/downloadTestPdf.php
@@ -1,5 +1,4 @@
 <?php
-/*
 ini_set('memory_limit', '1024M'); 
 
 ini_set('display_errors', 1);
@@ -9,7 +8,11 @@ error_reporting(E_ALL);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS"); 
 header("Access-Control-Allow-Headers: Content-Type");
-*/
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
 
 require 'vendor/autoload.php';
 require_once 'dompdf/autoload.inc.php';
@@ -17,10 +20,12 @@ require_once('internal/templates/Templates.php');
 
 use Dompdf\Dompdf;
 
+$jsonInput = file_get_contents('php://input');
+$requestData = json_decode($jsonInput, true);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  $html = $_POST['htmlContent'];
-  die($_POST['htmlContent']);
+  $html = $requestData['htmlContent'];
   $dompdf = new Dompdf();
   $dompdf->loadHtml($html);
   $dompdf->setPaper('A4', 'landscape');

207624afc8e055fe28757dbab767e577b1ec75cd
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-25 20:25:18 -0300

    apa
    apa

---
+++ editor/themes/5/index.php
@@ -0,0 +1,102 @@
+<html>
+  <head>
+    <title>John Wick's Resume</title>
+    <link
+      href="https://fonts.cdnfonts.com/css/glacial-indifference-2"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&amp;display=swap"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Playfair+Display&amp;display=swap"
+      rel="stylesheet"
+    />
+
+ <link
+      rel="stylesheet"
+      href="https://easyresumepulse.com/en/editor/themes/5/styles.css"
+      id="delete-this-css"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="box-1">
+        <h5 class="subtitle">CONTACT<br/> INFORMATION</h5>
+        <ul class="list-contact">
+          <li>35 years old.</li>
+          <li>johnwick@gmail.com</li>
+          <li>Tel: 1234567890</li>
+          <li>123 Continental Blvd</li>
+          <li>New York City, NY</li>
+        </ul>
+        <h5 class="subtitle">EDUCATION</h5>
+        <ul class="list-contact">
+          <li><b>Master Assassin Training</b></li>
+          <li>International Assassin Guild</li>
+          <li>2005-2009</li>
+        </ul>
+        <ul class="list-contact">
+          <li><b>High School Diploma</b></li>
+          <li>Assassin Academy</li>
+          <li>2000-2004</li>
+        </ul>
+        <h5 class="subtitle">COURSES AND LANGUAGES</h5>
+        <ul class="list-contact">
+          <li><b>Language: Russian</b></li>
+          <li>Fluent in speaking and writing.</li>
+        </ul>
+        <ul class="list-contact">
+          <li><b>Combat Training</b></li>
+          <li>Hand-to-hand combat and firearms proficiency.</li>
+        </ul>
+      </div>
+      <div class="box-2">
+        <h1>John</h1>
+        <h1>Wick</h1>
+        <h5 class="subtitle">MASTER ASSASSIN</h5>
+        <ul class="list-contact">
+          <li>
+            I am a highly skilled and disciplined professional with extensive
+            experience in handling complex tasks. Seeking opportunities that
+            challenge my expertise and abilities.
+          </li>
+        </ul>
+        <h5 class="subtitle">WORK EXPERIENCE</h5>
+        <ul class="list-experience">
+          <li>
+            <b>Assassin Specialist</b>
+            <p>Continental Corporation, January 2014 - Present</p>
+            <p>
+              Execution of high-profile contracts. Management of critical
+              missions and operations.
+            </p>
+          </li>
+          <li class="mt-3">
+            <b>Senior Hitman</b>
+            <p>Underworld Syndicate, August 2010 - December 2013</p>
+    
+            <p>
+              Coordinated hits and strategic eliminations. Led teams for
+              specialized missions.
+            </p>
+          </li>
+          <li class="mt-3">
+            <b>Apprentice Assassin</b>
+            <p>Assassin Academy, July 2009 - July 2010</p>
+           
+            <p>
+              Intensive training in combat techniques and stealth operations.
+              Mentored by seasoned assassins.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>

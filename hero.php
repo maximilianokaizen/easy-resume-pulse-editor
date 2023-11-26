1cef3ceff77fe3511434fb2ba2fc03d2e9598736
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-26 00:10:16 -0300

    asd
    asd

--- hero.php
+++ hero.php
@@ -3,12 +3,14 @@
   <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
     <div class="row justify-content-center">
       <div class="col-xl-7 col-lg-9 text-center">
-        <h1>Create Your Professional Resume Quickly, Easily, and Absolutely Free</h1>
-        <h2>No Hassle, No Cost. Craft Your Resume in Minutes!</h2>
+        <h1><i class="lni lni-pencil-alt"></i> Create Your Professional Resume Quickly, Easily, and Absolutely Free</h1>
+        <h2>No Hassle, No Cost. Craft Your Resume in Minutes! <i class="lni lni-bolt-alt"></i><i class="lni lni-bolt-alt"></i><i class="lni lni-bolt-alt"></i></h2>
       </div>
     </div>
+    <!--
     <div class="text-center">
       <a href="<?=$baseUrl?>/signin.php" class="btn-get-started btn-get-green scrollto">Get Started for Free</a>
     </div>
+-->
   </div>
 </section><!-- End Hero -->

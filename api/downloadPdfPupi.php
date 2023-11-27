1e00e714c2218bd617b1b4d52ddeea55917c0c50
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-27 11:40:26 -0300

    Update downloadPdfPupi.php

--- api/downloadPdfPupi.php
+++ api/downloadPdfPupi.php
@@ -1,38 +1,56 @@
 <?php
 
-// Datos HTML para enviar al servicio en formato JSON
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+}
+
 $htmlData = '<html><body><h1>Hello, World!</h1><p>This is a test HTML content.</p></body></html>';
 $jsonData = json_encode(array('html' => $htmlData));
 
-// URL del servicio que genera el PDF
 $url = 'http://easyresumecreator.online/pdf';
 
-// Configuración de la solicitud cURL
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonData);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 
-// Ejecutar la solicitud cURL
 $response = curl_exec($curl);
-
-// Verificar si la solicitud fue exitosa
 $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
 if ($httpCode === 200) {
-    $pdfFilePath = '/home/easyre/public_html/en/resumes/generated_pdf.pdf';
+    $pdfFileName = generateRandomString() . '.pdf'; // Nombre aleatorio para el PDF
+    $pdfFilePath = '/home/easyre/public_html/en/resumes/' . $pdfFileName;
+    
     $success = file_put_contents($pdfFilePath, $response);
     
     if ($success !== false) {
-        echo 'PDF generado y guardado en: ' . $pdfFilePath;
+        $output = array(
+            'success' => true,
+            'message' => 'PDF generado y guardado correctamente.',
+            'filePath' => 'https://easyresumepulse.com/en/resumes/' . $pdfFileName
+        );
     } else {
         $error = error_get_last();
-        echo 'Error al guardar el archivo: ' . $error['message'];
+        $output = array(
+            'success' => false,
+            'message' => 'Error al guardar el archivo PDF: ' . $error['message']
+        );
     }
 } else {
-    echo 'Error al obtener el PDF. Código de estado HTTP: ' . $httpCode;
+    $output = array(
+        'success' => false,
+        'message' => 'Error al obtener el PDF. Código de estado HTTP: ' . $httpCode
+    );
 }
 
-// Cerrar la sesión cURL
 curl_close($curl);
+
+header('Content-Type: application/json');
+echo json_encode($output);
 ?>

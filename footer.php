fd097fdbdb2ca0157e196e0482d35631c422a808
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-28 11:18:03 -0300

    comment
    comment

--- footer.php
+++ footer.php
@@ -41,8 +41,10 @@
     </div>
   </div>
   <div class="social-links text-center text-md-right pt-3 pt-md-0">
+    <!--
     <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
     <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+  -->
   </div>
 </div>
 </footer><!-- End Footer -->

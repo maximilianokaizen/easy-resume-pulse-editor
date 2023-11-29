0c3969179ff2b874b0b1dc045404fcba0525fcec
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-29 09:29:22 -0300

    simple
    simple

--- head.php
+++ head.php
@@ -1,3 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RE3Y27JRY9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RE3Y27JRY9');
+</script>
 <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta name="robots" content="noindex">

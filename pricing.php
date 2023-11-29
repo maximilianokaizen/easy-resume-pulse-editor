87242e862d0ca3ede80e47fe81d32d1a58452e24
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-29 09:22:00 -0300

    Update pricing.php

--- pricing.php
+++ pricing.php
@@ -21,7 +21,7 @@
           <li>Effortlessly highlight your skills, experiences, and achievements</li>
           </ul>
           <p style="margin-top: 20px;">Consider supporting our project! Your donation helps us maintain this service free and accessible to everyone. Your contribution supports server costs, development, and ongoing improvements, ensuring you can create and save your CVs at no cost. Every donation matters to continue providing you with the best possible experience.</p>
-          <a href="https://www.buymeacoffee.com/easyresumepulse" target="_blank" class="btn-buy" style="font-weight: bold;">Support the project :D</a>
+          <a href="https://www.buymeacoffee.com/easyresumepulse" target="_blank" class="btn-buy" style="font-weight: bold;">Support the project</a>
           <br/><br/>        
         </div>
       </div>

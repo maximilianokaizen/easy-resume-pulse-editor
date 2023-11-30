3ee2c91790d55d04dfde38b7d7bde9f0d626e512
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-30 11:23:29 -0300

    Update hero.php

--- hero.php
+++ hero.php
@@ -1,9 +1,11 @@
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="d-flex align-items-center" style="position:relative">
   <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+    <!--
     <div class="hero-image-left" style="position:absolute">
         <img src="pngwing.png" alt="" style="width:200px" />
     </div>
+-->
     <div class="row justify-content-center">
       <div class="col-xl-7 col-lg-9 text-center">
         <h1><i class="lni lni-bolt-alt"></i> Create Your Professional Resume Quickly, Easily, and Absolutely Free</h1>

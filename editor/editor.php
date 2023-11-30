af33784a98745ef4ed58bf4c1b80656281edf840
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-11-30 14:03:53 -0300

    Update editor.php

--- editor/editor.php
+++ editor/editor.php
@@ -1818,7 +1818,7 @@ $(function() {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ html: htmlContent, template: templateId, width : dimensiones.width, height :  dimensiones.height})
+        body: JSON.stringify({ html: htmlContent, template: templateId, width : dimensiones.width, height :  dimensiones.height + 500})
     })
         .then(response => response.json())
         .then(data => {

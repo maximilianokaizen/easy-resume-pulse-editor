5478b145a9dc40e7b34fb9a7270f851f0ca859fc
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-12-02 20:33:30 -0300

    Update header-upgrade.php

--- header-upgrade.php
+++ header-upgrade.php
@@ -11,9 +11,9 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="<?=$baseUrl?>/panel.php">  Back To Panel </a></li>
-          <li><a class="getstarted scrollto show-ui" href="#" id="signOut">
-          <i class="fas fa-door-open"></i>
-&nbsp; Logout
+          <li><a class="getstarted scrollto show-ui" href="#" id="signOut"> <i class="fas fa-door-open"></i>
+            &nbsp;
+             Logout
         </a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>

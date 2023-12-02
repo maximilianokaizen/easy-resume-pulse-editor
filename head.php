11b8d0602bdabeb38af087c197d1f653de527798
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-12-02 18:59:30 -0300

    se
    se

--- head.php
+++ head.php
@@ -16,24 +16,24 @@
   <meta content="" name="keywords">
 
 
-  <!-- For IE 9 and below -->
-<!--[if IE]>
-  <link rel="shortcut icon" href="<?=$urlBase?>/en/assets/favicon.ico">
-<![endif]-->
-
-<!-- For IE 10+ and modern browsers -->
-<link rel="icon" type="image/x-icon" href="<?=$urlBase?>/en/assets/favicon.ico">
-<link rel="icon" type="image/png" sizes="16x16" href="<?=$urlBase?>/en/assets/favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?=$urlBase?>/en/assets/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?=$urlBase?>/en/assets/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?=$urlBase?>/en/assets/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?=$urlBase?>/en/assets/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?=$urlBase?>/en/assets/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?=$urlBase?>/en/assets/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?=$urlBase?>/en/assets/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?=$urlBase?>/en/assets/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?=$urlBase?>/en/assets/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?=$urlBase?>/en/assets/android-icon-192x192.png">
 <link rel="icon" type="image/png" sizes="32x32" href="<?=$urlBase?>/en/assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?=$urlBase?>/en/assets/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?=$urlBase?>/en/assets/favicon-16x16.png">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?=$urlBase?>/en/assets/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
 
-<!-- For Android -->
-<link rel="icon" type="image/png" sizes="192x192" href="<?=$urlBase?>/en/assets/android-chrome-192x192.png">
-<link rel="icon" type="image/png" sizes="512x512" href="<?=$urlBase?>/en/assets/android-chrome-512x512.png">
-
-<!-- For iOS -->
-<link rel="apple-touch-icon" href="<?=$urlBase?>/en/assets/apple-touch-icon.png">
-
-
+ 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 

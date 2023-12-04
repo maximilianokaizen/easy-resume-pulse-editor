d80ddb4103eff83f1580867e0a4e51a75963f938
maximilianokaizen <maximiliano.rossi@kaizenpulse.com>
2023-12-04 14:29:27 -0300

    Update upgrade-plan.php

--- upgrade-plan.php
+++ upgrade-plan.php
@@ -5,6 +5,7 @@ include_once 'init-zero.php';
 <html lang="en">
 <head>
 <?php include_once 'head.php'; ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
 <?php include_once 'header-upgrade.php'; ?>

eb5e5185e8b9ab03efdeb80f2a381fb7645a557a
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-12-05 14:23:38 -0300

    Update loadResume.php

--- api/resumes/loadResume.php
+++ api/resumes/loadResume.php
@@ -77,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $html = $resume[0]['html'];
         
         //die('template => ' . $template);
-        if ($imageUrl !== '' && ($template == 48 || $template == 50 || $template == 51)){
+        if ($imageUrl !== ''){
             $dom = new DOMDocument();
             $dom->loadHTML($html);
             $elements = $dom->getElementsByClassName('img-profile-image');

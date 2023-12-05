9fc8b252179061b99550cdc2a0570c8ad8029db3
maximilianokaizen <148482605+maximilianokaizen@users.noreply.github.com>
2023-12-05 14:53:23 -0300

    Update loadResume.php

--- api/resumes/loadResume.php
+++ api/resumes/loadResume.php
@@ -90,7 +90,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 render($updatedHTML, $imageUrl, $template);
             }
             */
-        render($html, $imageUrl, $template);
+        render($html, $template, $image);
 
     } catch (Exception $e) {
         http_response_code(500);
